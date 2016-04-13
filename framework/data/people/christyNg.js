2b1ef78dc57a147bcfe1ba3aa4fcd80b49054b34
Raymond Chan <raymondschan@gmail.com>
2016-04-12 22:26:21 -0700

    Adding Christy and Eva photos

--- framework/data/people/christyNg.js
+++ framework/data/people/christyNg.js
@@ -3,10 +3,10 @@ data.people.push({
 	"type":"Maid of Honor",
 	"imgs":[
 		"christyNg_01.png",
-		"christyNg_02.png",
-		"christyNg_03.png"
+		"christyNg_02.png"
 	],
 	"thumbnails": [
+		"christyNg01_thumbnail.png"
 	],
 	"about":[
 		"My little sister Christy is a big ball of energy, very friendly, and always willing to help a friend or family. This is probably why she works at Sales Coordinator at Silver Cloud. I love her even though we don't always share the same interest but it helps she eats the chicken feet while I help eat the leg and thigh. =) She is my friend, who I can call to complain or celebrate about anything and count on her support, strength and laughter. "
